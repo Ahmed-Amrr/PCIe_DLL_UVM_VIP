9f8dce756076d4ebef6a8c5ec5e133688ed6da51
EmanYasser <emanyaser811@gmail.com>
2026-03-15 02:53:00 +0200

    Update lpif_if.sv

--- test code/lpif_if.sv
+++ test code/lpif_if.sv
@@ -4,39 +4,22 @@ interface lpif_if (
 
     bit        pl_lnk_up;   // Indicates that the physical link is up and active
 
-    bit        lp_irdy ;   // Transmitter is ready to send data
     bit        lp_valid;   // Indicates that lp_data contains valid data
     bit [63:0] lp_data ;   // 64-bit data bus from Link Partner (DLLP/TLP data)
 
-    bit        pl_trdy ;   // Physical Layer Ready: receiver ready to accept data
     bit        pl_valid;   // Indicates that pl_data contains valid data from PL
     bit [63:0] pl_data ;   // 64-bit data bus from Physical Layer
-
 
     // driver clocking block
     clocking drv_cb (@posedge lclk);
         default output #0;
-
-        output pl_lnk_up;
-        output lp_irdy;
-        output lp_valid;
-        output lp_data;
-        output pl_trdy;
-        output pl_valid;
-        output pl_data;
+        output pl_lnk_up, lp_valid, lp_data, pl_data, pl_valid;
     endclocking
 
     // monitor clocking block
     clocking mon_cb (@posedge lclk);
         default input #1step;
-
-        input pl_lnk_up;
-        input lp_irdy;
-        input lp_valid;
-        input lp_data;
-        input pl_trdy;
-        input pl_valid;
-        input pl_data;
+        input pl_lnk_up, lp_valid, lp_data, pl_data, pl_valid;
     endclocking
 
 endinterface // lpif_if
