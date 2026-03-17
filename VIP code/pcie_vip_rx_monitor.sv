9119dd3a964fe061d0bac8a19f4fbf1f01d210ef
AHMED-ABDELLATIF56 <144045090+AHMED-ABDELLATIF56@users.noreply.github.com>
2026-03-17 22:24:55 +0200

    GL edits

--- VIP code/pcie_vip_rx_monitor.sv
+++ VIP code/pcie_vip_rx_monitor.sv
@@ -1,49 +1,49 @@
 `ifndef PCIE_VIP_RX_MONITOR
 `define PCIE_VIP_RX_MONITOR
 
-class pcie_vip_rx_monitor extends uvm_monitor;
-/*-------------------------------------------------------------------------------
--- UVM Factory register
--------------------------------------------------------------------------------*/
-	// Provide implementations of virtual methods such as get_type_name and create
-	`uvm_component_utils(pcie_vip_rx_monitor)
+	class pcie_vip_rx_monitor extends uvm_monitor;
+	/*-------------------------------------------------------------------------------
+	-- UVM Factory register
+	-------------------------------------------------------------------------------*/
+		// Provide implementations of virtual methods such as get_type_name and create
+		`uvm_component_utils(pcie_vip_rx_monitor)
 
-/*-------------------------------------------------------------------------------
--- Interface, port, fields
--------------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------------
+	-- Interface, port, fields
+	-------------------------------------------------------------------------------*/
 
-    virtual lpif_if lpif_vif;
-    pcie_dllp_seq_item   seq_item_rx_mon;
-	uvm_analysis_port #(pcie_dllp_seq_item) rx_mon_ap;
+	    virtual lpif_if lpif_vif;
+	    pcie_dllp_seq_item   seq_item_rx_mon;
+		uvm_analysis_port #(pcie_dllp_seq_item) rx_mon_ap;
 
-/*-------------------------------------------------------------------------------
--- Functions
--------------------------------------------------------------------------------*/
-	// Constructor
-	function new(string name = "pcie_vip_rx_monitor", uvm_component parent=null);
-		super.new(name, parent);
-	endfunction : new
+	/*-------------------------------------------------------------------------------
+	-- Functions
+	-------------------------------------------------------------------------------*/
+		// Constructor
+		function new(string name = "pcie_vip_rx_monitor", uvm_component parent=null);
+			super.new(name, parent);
+		endfunction : new
 
-	function void build_phase(uvm_phase phase);
-		super.build_phase(phase);
-		rx_mon_ap=new("rx_mon_ap", this);
-	endfunction : build_phase
+		function void build_phase(uvm_phase phase);
+			super.build_phase(phase);
+			rx_mon_ap=new("rx_mon_ap", this);
+		endfunction : build_phase
 
-	task run_phase(uvm_phase phase);	//check the names for vif & seq_item & variables
-		super.run_phase(phase);
-		forever begin
-			seq_item_rx_mon=pcie_dllp_seq_item::type_id::create("seq_item_rx_mon");
-            @(lpif_vif.mon_cb);
-            if (lpif_vif.pl_valid) begin //ask abedllatif
-				seq_item_rx_mon.dllp=lpif_vif.pl_data;
-				seq_item_rx_mon.pl_valid=lpif_vif.pl_valid;
-				seq_item_rx_mon.pl_lnk_up=lpif_vif.pl_lnk_up;
-				tx_mon_ap.write(seq_item_rx_mon);
-            end
-			/*`uvm_info("run_phase", seq_item_rx_mon.convert2string(), UVM_HIGH)*/
-		end
-	endtask : run_phase
+		task run_phase(uvm_phase phase);	//check the names for vif & seq_item & variables
+			super.run_phase(phase);
+			forever begin
+				seq_item_rx_mon=pcie_dllp_seq_item::type_id::create("seq_item_rx_mon");
+	            @(lpif_vif.mon_cb);
+	            if (lpif_vif.pl_valid) begin //ask abedllatif
+					seq_item_rx_mon.dllp=lpif_vif.pl_data;
+					seq_item_rx_mon.pl_valid=lpif_vif.pl_valid;
+					seq_item_rx_mon.pl_lnk_up=lpif_vif.pl_lnk_up;
+					tx_mon_ap.write(seq_item_rx_mon);
+	            end
+				/*`uvm_info("run_phase", seq_item_rx_mon.convert2string(), UVM_HIGH)*/
+			end
+		endtask : run_phase
 
-endclass : pcie_vip_rx_monitor
+	endclass : pcie_vip_rx_monitor
 
 `endif 
--- test code/glue_logic_driver.sv
+++ test code/glue_logic_driver.sv
@@ -1,17 +1,13 @@
-`ifndef GLUE_LOGIC_DRIVER
-`define GLUE_LOGIC_DRIVER
 class glue_logic_driver extends uvm_driver;
 
-    // UVM Factory register
     `uvm_component_utils(glue_logic_driver)
 
-    virtual lpif_if    lpif_vif;
-    pcie_seq_item      s_item  ;
-    pcie_top_cfg       cfg     ;
+    virtual lpif_if lpif_vif;
+    pcie_dllp_seq_item s_item;
+    pcie_top_cfg cfg;
 
-    uvm_tlm_analysis_fifo #(pcie_seq_item) fifo_mon;
+    uvm_tlm_analysis_fifo #(pcie_dllp_seq_item) fifo_mon;
 
-    // UVM Factory register
     function new(string name = "glue_logic_driver", uvm_component parent = null);
         super.new(name, parent);
     endfunction //new()
@@ -23,20 +19,16 @@ class glue_logic_driver extends uvm_driver;
             `uvm_fatal("CFG", "Config object not found")
     endfunction
 
-    task run_phase(uvm_phase phase);
+    task run_phase();
         super.run_phase(phase);
         fifo_mon.get(s_item);
         forever begin
-            // Get transcation from monitor
             fifo_mon.get(s_item);
             @(lpif_vif.drv_cb)
-
-            // Check Physical linkup signal
             if (cfg.link_down_test == 0) begin
                 lpif_vif.drv_cb.pl_lnk_up <= 1;
-                if (s_item.lp_valid) begin
-                    lpif_vif.drv_cb.pl_data <= s_item.lp_data;
-                end
+                lpif_vif.drv_cb.pl_data <= s_item.lp_data;
+
                 if (cfg.pl_valid_off) begin
                     lpif_vif.drv_cb.pl_valid <= 0;
                 end else begin
@@ -48,4 +40,3 @@ class glue_logic_driver extends uvm_driver;
         end
     endtask
 endclass 
-`endif
